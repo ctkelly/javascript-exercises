3713cb6ebc7ec4883fa4bdc5fb91bf6ced40a97d
Chloe Kelly <chloetay3@gmail.com>
2020-09-25 17:09:36 -0400

    complete helloWorld and repeatString tests

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,25 @@
-const repeatString = function() {
 
-}
+function randomNum() {
+  const number = Math.floor(Math.random() * 10);
+  return number
+} 
+
+  const repeatString = function(string, n) {
+    let newString = '';
+    if (n == randomNum()) {
+    for (let i = 0; i < randomNum; i++) {
+    newString += string;
+    }
+    return newString;
+    } else if (n < 0) {
+    return 'ERROR';
+    } else if (n >= 0) {
+    for (let i = 0; i < n; i++) {
+    newString +=string;
+    }
+    return newString;
+    }
+    } 
+
 
 module.exports = repeatString
