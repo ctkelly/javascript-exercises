8598094fced8d0ccccaeb355447c2cabf218c3d8
Chloe Kelly <chloetay3@gmail.com>
2020-10-02 20:42:27 -0400

    Completed tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(F) {
+  let unroundedC = (F - 32) * 5 / 9;
+  let roundedCTemp = Math.round(unroundedC * 10) / 10;
+  return roundedCTemp;
 }
 
-const ctof = function() {
-
+const ctof = function(C) {
+  let unroundedF = (C * 9 / 5) + 32;
+  let roundedFTemp = Math.round(unroundedF * 10) / 10;
+  return roundedFTemp;
 }
 
 module.exports = {
