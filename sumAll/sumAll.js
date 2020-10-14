7e46ce201c5880c3029ffc0d83ada3e1b5cd1ac4
Chloe Kelly <chloetay3@gmail.com>
2020-10-13 21:21:33 -0400

    Complete sumAll tests

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
-}
+const sumAll = function(num1, num2) {
+    let array = [];
+    let step = 1;
+    if (typeof num1 === 'string' || typeof num2 === 'string') {
+        return 'ERROR';
+    } else if (isNaN(num1) === true || isNaN(num2) === true) {
+        return 'ERROR';
+    } else if (num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    } else if (num1 < num2) {
+        for (let i = num1; i <= num2; i += step) {
+          array.push(i);  
+        }
+        return (array.length / 2) * (num1 + num2);
+    } else if (num1 > num2) {
+        for (let i = num2; i <= num1; i += step) {
+            array.push(i);
+        }
+        return (array.length / 2) * (num1 + num2);
+    }
+} 
 
 module.exports = sumAll
