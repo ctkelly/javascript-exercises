08df8bef91aa5b82f4ee33e573f3bdf52efdec5b
Chloe Kelly <chloetay3@gmail.com>
2021-02-15 18:31:28 -0500

    Complete calculator tests

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,26 +1,47 @@
-function add () {
-	
+function add (num1, num2) {
+	return num1 + num2;
 }
 
-function subtract () {
-	
+function subtract (minuend, subtrahend) {
+	return minuend - subtrahend;
 }
 
-function sum () {
+function sum (array) { 
+	let initialValue = 0;
 	
+	let reducer = (accumulator, item) => {
+		return accumulator + item;
+	}
+
+	let sum = array.reduce(reducer, initialValue);
+
+	return sum;
+} 
+
+function multiply (array) {
+	let initialValue = 1;
+	
+	let reducer = (accumulator, item) => {
+		return accumulator * item;
+	}
+
+	let product = array.reduce(reducer, initialValue);
+
+	return product;
+	
+} 
+
+function power(base, exponent) {
+	return Math.pow(base, exponent);
 }
 
-function multiply () {
-	
-}
-
-function power() {
-	
-}
-
-function factorial() {
-	
-}
+function factorial(n) {
+	if (n == 0) {
+		return 1;
+	} else {
+		return (n != 1) ? n * factorial(n - 1) : 1;
+	}	
+} 
 
 module.exports = {
 	add,
