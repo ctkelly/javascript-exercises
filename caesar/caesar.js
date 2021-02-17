1f7e6d699c4400bb5b974de6d9a40a8a6b3c862a
Chloe Kelly <chloetay3@gmail.com>
2021-02-17 13:48:14 -0500

    Initial commit, only works for test #1

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,4 +1,32 @@
-const caesar = function() {
+const caesar = function(string, shiftValue) {
+  
+  const alphabet = [
+      'A', 'B', 'C', 'D', 'E', 'F',
+      'G', 'H', 'I', 'J', 'K', 'L',
+      'M', 'N', 'O', 'P', 'Q', 'R',
+      'S', 'T', 'U', 'V', 'W', 'X',
+      'Y', 'Z'
+    ];
+
+  //const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split(); NO  
+  let arrayOfLetters = string.split('');
+
+  function encrypt(letter) {
+    
+    let shift = shiftValue;
+
+    if (alphabet.includes(letter.toUpperCase())) {
+      let position = alphabet.indexOf(letter.toUpperCase());
+      let newPosition = (position + shift) % 26;
+      return alphabet[newPosition];
+
+    } else {
+      return letter;
+    }
+  }
+
+  let encryptedString = arrayOfLetters.map(letter => encrypt(letter)).join('');
+  return encryptedString;
 
 }
 
