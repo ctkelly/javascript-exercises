9605cdf19e79740313dd8e6e2f002adaf224dc16
Chloe Kelly <chloetay3@gmail.com>
2021-02-17 19:11:38 -0500

    Refactor for negative shifts but without original case

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -8,26 +8,63 @@ const caesar = function(string, shiftValue) {
       'Y', 'Z'
     ];
 
-  //const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split(); NO  
-  let arrayOfLetters = string.split('');
+  //const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split(); NO, USE THE OTHER ONE  
 
-  function encrypt(letter) {
-    
-    let shift = shiftValue;
+  let strToEncrypt = string.split('');
+  
+  while (shiftValue < 0) {
+    shiftValue += 26;
+  }
 
-    if (alphabet.includes(letter.toUpperCase())) {
-      let position = alphabet.indexOf(letter.toUpperCase());
-      let newPosition = (position + shift) % 26;
+  function encrypt(character) {
+
+    if (alphabet.includes(character.toUpperCase())) {
+      let position = alphabet.indexOf(character.toUpperCase());
+      let newPosition = (position + shiftValue) % 26;
       return alphabet[newPosition];
 
     } else {
-      return letter;
+      return character;
     }
   }
 
-  let encryptedString = arrayOfLetters.map(letter => encrypt(letter)).join('');
+  let encryptedString = strToEncrypt.map(character => encrypt(character)).join('');
   return encryptedString;
 
-}
+} 
+
+
+
+
+
+
+/*function caesar(string, shiftValue) {
+  
+  const alphabet = [
+    'A', 'B', 'C', 'D', 'E', 'F',
+    'G', 'H', 'I', 'J', 'K', 'L',
+    'M', 'N', 'O', 'P', 'Q', 'R',
+    'S', 'T', 'U', 'V', 'W', 'X',
+    'Y', 'Z'
+  ];
+
+  let strToEncrypt = string.split('');
+
+  function encrypt(character) {
+    
+    let shiftValue = shiftValue;
+
+    if (shiftValue < 0 && alphabet.includes(character.toUpperCase())) {
+      let position = alphabet.indexOf(character.toUpperCase());
+      let newPosition = 26 + (shiftValue % 26);
+      return alphabet[newPosition];
+
+    } else {
+      return character;
+    }
+  }
+  let encryptedString = strToEncrypt.map(character => encrypt(character)).join('');
+  return encryptedString;  
+}*/ 
 
 module.exports = caesar
