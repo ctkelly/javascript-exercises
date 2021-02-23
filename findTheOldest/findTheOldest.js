d2d2d00ae8fe60969c438d73160145e4f3551e15
Chloe Kelly <chloetay3@gmail.com>
2021-02-23 16:07:19 -0500

    Complete findTheOldest tests

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-let findTheOldest = function() {
-
+function findAge(deathDate, birthDate) {
+  let thisYear = new Date().getFullYear();
+  return (deathDate || thisYear) - birthDate;
 }
+
+let findTheOldest = function(people) {
+  return people.reduce((oldest, currentPerson) => {
+    let oldestAge = findAge(oldest.yearOfDeath, oldest.yearOfBirth);
+    let currentAge = findAge(currentPerson.yearOfDeath, currentPerson.yearOfBirth);
+    return oldestAge > currentAge ? oldest : currentPerson;
+  })
+} 
 
 module.exports = findTheOldest
