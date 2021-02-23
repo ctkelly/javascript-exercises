f7a90d361009762a2f6feaf68ecd46bc77653688
Chloe Kelly <chloetay3@gmail.com>
2021-02-23 16:05:11 -0500

    Complete caesar tests

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,36 @@
 const caesar = function(string, shiftValue) {
   
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+  let strToEncrypt = string.split('');
+  
+  while (shiftValue < 0) {
+    shiftValue += 26;
+  }
+
+  function encrypt(character) {
+
+    if (alphabet.includes(character.toLowerCase())) {
+      let position = alphabet.indexOf(character.toLowerCase());
+      let newPosition = (position + shiftValue) % 26;
+      if (character === character.toUpperCase()) {
+        return alphabet[newPosition].toUpperCase();
+      }
+      return alphabet[newPosition];
+    }
+    return character;
+  
+  }
+
+  let encryptedString = strToEncrypt.map(character => encrypt(character)).join('');
+  return encryptedString;
+
+} 
+
+
+
+/*const caesar = function(string, shiftValue) {
+  
   const alphabet = [
       'A', 'B', 'C', 'D', 'E', 'F',
       'G', 'H', 'I', 'J', 'K', 'L',
@@ -31,40 +62,8 @@ const caesar = function(string, shiftValue) {
   let encryptedString = strToEncrypt.map(character => encrypt(character)).join('');
   return encryptedString;
 
-} 
-
-
-
-
-
-
-/*function caesar(string, shiftValue) {
-  
-  const alphabet = [
-    'A', 'B', 'C', 'D', 'E', 'F',
-    'G', 'H', 'I', 'J', 'K', 'L',
-    'M', 'N', 'O', 'P', 'Q', 'R',
-    'S', 'T', 'U', 'V', 'W', 'X',
-    'Y', 'Z'
-  ];
-
-  let strToEncrypt = string.split('');
-
-  function encrypt(character) {
-    
-    let shiftValue = shiftValue;
-
-    if (shiftValue < 0 && alphabet.includes(character.toUpperCase())) {
-      let position = alphabet.indexOf(character.toUpperCase());
-      let newPosition = 26 + (shiftValue % 26);
-      return alphabet[newPosition];
-
-    } else {
-      return character;
-    }
-  }
-  let encryptedString = strToEncrypt.map(character => encrypt(character)).join('');
-  return encryptedString;  
 }*/ 
+
+
 
 module.exports = caesar
